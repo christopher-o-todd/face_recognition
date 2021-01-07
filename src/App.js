02d71a2500cd1bab7736f1a4fa0ca37483003e74
Christopher Todd <Christopher.Todd@edu.sait.ca>
2021-01-06 17:19:20 -0700

    fixed image fetch url

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ class App extends Component {
     .then(response => response.json())
     .then(response => {
       if (response) {
-        fetch('https://arcane-eyrie-88621.herokuapp.com:3000/image', {
+        fetch('https://arcane-eyrie-88621.herokuapp.com/image', {
           method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
